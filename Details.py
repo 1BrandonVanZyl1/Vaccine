779f678c591756ed8857e090615b152c51ca0a2c
1BrandonVanZyl1 <89069026+1BrandonVanZyl1@users.noreply.github.com>
2021-08-18 09:59:03 +0200

    Add files via upload

---
+++ Details.py
@@ -0,0 +1,13 @@
+id = input("ID: ")
+surname=input("Surname:")
+firstName=("Name: ")
+gender =input("Gender m/f: ")
+age =input("Age: ")
+province=input ("Province: ")
+
+
+if(age > 30 ):
+    print("You are legible for the vaccine!")
+
+else:
+    print("You do not qualify for the vaccine!")
